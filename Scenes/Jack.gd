ef8461e102415c417f5fd1b97f9991801ed23e77
Nutriz <jerome.gully0@gmail.com>
2020-05-13 22:24:04 +0200

    Added white sound for jack

--- Scenes/Jack.gd
+++ Scenes/Jack.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var picked
+var picked setget picked_set
 var connected_input setget connected_set
 onready var initial_position = position
 
@@ -27,8 +27,8 @@ func refresh_cable_position():
 func _on_PickArea_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT:
-			picked = event.pressed
-			if not event.pressed and not $Area.get_overlapping_areas().empty():
+			self.picked = event.pressed
+			if not picked and not $Area.get_overlapping_areas().empty():
 				var input_overlapped = $Area.get_overlapping_areas().front()
 				if (name == "JackA" and input_overlapped.is_in_group("inputsA")) or (name == "JackB" and input_overlapped.is_in_group("inputsB") and Global.switch_button.pressed):
 					var input = $Area.get_overlapping_areas().front().get_parent()
@@ -44,7 +44,17 @@ func reset_position():
 
 func connected_set(new_value):
 	if new_value != null:
+		$JackIn.play()
+		yield(get_tree().create_timer(0.7), "timeout")
 		Events.emit_signal("jack_connected", name, new_value)
+		connected_input = new_value
 	elif connected_input != null:
 		Events.emit_signal("jack_disconnected", name, connected_input)
-	connected_input = new_value
+		connected_input = new_value
+
+func picked_set(new_value):
+	picked = new_value
+	if picked:
+		$JackBeforePlug.play()
+	else:
+		$JackBeforePlug.stop()
